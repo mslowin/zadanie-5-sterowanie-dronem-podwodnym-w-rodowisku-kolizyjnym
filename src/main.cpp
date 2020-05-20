d3aae1a5483627669584b33e93f39871a129f3ad
Michal <254018@student.pwr.edu.pl>
2020-05-20 20:39:53 +0200

    Dodano możliwość opadania i wznoszenia się dronem o zadany kąt przy przesunięciu

--- src/main.cpp
+++ src/main.cpp
@@ -13,80 +13,15 @@
 #include <chrono>
 #include <thread>
 
+//do funkcji trygonometrycznych:
+#include <cmath>
+
 using namespace std;
 
 using namespace std::this_thread;
 using namespace std::chrono;
 //const std::string kModelCuboid("bryly/model2.dat");
 const string kDroneFile("bryly/drone.dat");
-
-/*class Prostopadloscian{
-    std::vector<Wektor3D> points;
-    Wektor3D translation;
-    MacierzRot3D rotation;
-    double angle;
-
-public:
-    Prostopadloscian();
-    void rysuj(std::string filename) const;
-    void translate(const Wektor3D& change)
-    {
-        translation = translation + change;
-    }
-    void rotateZ(int kat)
-    {
-        MacierzRot3D mac;
-        mac.UstawRotX_st(0);
-        mac.UstawRotY_st(0);
-        mac.UstawRotZ_st(kat);
-        //rotation = mac;
-        for (unsigned i = 0; i < points.size(); ++i)
-        {
-          points[i] = mac * points[i];
-        }
-    }
-};
-
-Prostopadloscian::Prostopadloscian(): angle{0}
-{
-    ifstream inputFile;
-    inputFile.open(kModelCuboid);
-    if(!inputFile.is_open())
-    {
-        cerr << "Unable to load model Cuboid file!" 
-             << endl;
-        return;
-    }
-
-    Wektor3D point;
-    while(inputFile >> point)
-    {
-        points.push_back(point);
-    }
-    inputFile.close();
-}
-
-void Prostopadloscian::rysuj(std::string filename) const
-{
-    ofstream outputFile;
-    outputFile.open(filename);
-    if(!outputFile.is_open())
-    {
-        cerr << "Unable to open drone file!" << endl;
-        return;
-    }
-    for(unsigned i = 0; i < points.size(); ++i)
-    {
-      
-      //sleep_until(system_clock::now() + seconds(1));
-        outputFile << points[i] + translation << endl;
-        //sleep_for(nanoseconds(10000));
-        if(i % 4 == 3) // triggers after every 4 points
-        {
-            outputFile << "#\n\n";
-        }
-    }
-}*/
 
 int main()
 {
@@ -121,9 +56,11 @@ int main()
   cout << "#   k - Zakoncz działanie programu  #\n"; /*koniec dzialania programu*/
   cout << "#####################################\n";
 
-  char wybor;
-  int a;
-  int odlegloscX;
+  char wybor;           //wybor w menu
+  int a;                //kąt obrotu
+  int b;                //kąt opadania/wznoszenia
+  int odlegloscZ;       //odległość opadania/wznoszenia
+  int odlegloscX;       //odległość przesunięcia 
 
   do
   {
@@ -135,14 +72,17 @@ int main()
       {
           cout << endl
                << "Podaj wartość kąta (wznoszenia/opadania) w stopniach" << endl;
-          cout << "Wartość kąta:     //JESZCZE NIE DZIAŁA" << endl;
+          cout << "Wartość kąta:   ";
+          cin >> b;
           cout << endl
                << "Podaj wartość odległości, na którą ma się przemiescić dron" << endl;
           cout << "Wartość odległości:   ";
           cin >> odlegloscX;
           cout << endl;
 
-          Wektor3D change(odlegloscX, 0, 0);
+          odlegloscZ = odlegloscX * tan(b*M_PI/180);
+
+          Wektor3D change(odlegloscX, 0, odlegloscZ);
 
           for (int i = 0; i < 20; i++)
           {
