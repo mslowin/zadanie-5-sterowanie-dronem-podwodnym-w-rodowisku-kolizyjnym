27ea605c027eb0bec657dd0c06aab9842d573813
Michal <254018@student.pwr.edu.pl>
2020-05-20 20:23:45 +0200

    Dodano menu i zautomatyzowano sterowanie dronem Nadal brakuje opadania i wznoszenia przy poruszaniu

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@
 #include <chrono>
 #include <thread>
 
-
 using namespace std;
+
 using namespace std::this_thread;
 using namespace std::chrono;
 //const std::string kModelCuboid("bryly/model2.dat");
@@ -92,7 +92,6 @@ int main()
 {
   Prostopadloscian cuboid; // To tylko przykladowe definicje zmiennej
   PzG::LaczeDoGNUPlota Lacze;
-  char c;
 
   Lacze.Inicjalizuj(); // Tutaj startuje gnuplot.
   Lacze.DodajNazwePliku(kDroneFile.c_str(), PzG::RR_Ciagly, 1);
@@ -110,41 +109,95 @@ int main()
   Lacze.DodajNazwePliku("bryly/pow_wody.dat");
 
   Lacze.Rysuj(); // Gnuplot rysuje to co jest w pliku
-  cout << "Nacisnij ENTER, aby kontynuowac ... " << flush;
-  cin >> noskipws >> c;
 
+  cout << endl
+       << endl
+       << "#####################################\n";
+  cout << "#               MENU:               #\n";
+  cout << "#   r - Zadaj ruch na wprost        #\n"; /*przemieszcza drona o zadaną odległość, wznosząc go lub obniżając*/
+  cout << "#   o - Zadaj zmianę orientacji     #\n"; /*obraca drona według osi Z*/
+  cout << "#   m - wyświetl menu               #\n"; /*wyswietla ponownie menu opcji*/
+  cout << "#                                   #\n";
+  cout << "#   k - Zakoncz działanie programu  #\n"; /*koniec dzialania programu*/
+  cout << "#####################################\n";
 
-  /*for (int i = 0; i < 16; i++)
+  char wybor;
+  int a;
+  int odlegloscX;
+
+  do
   {
-      cuboid.rotateZ(90/16);
-      cuboid.rysuj(kDroneFile);
-      Lacze.Rysuj();
-      chrono::milliseconds timespan(50);
-      this_thread::sleep_for(timespan);
-  }*/
+      cout << "Twoj wybor (m - menu):  ";
+      cin >> wybor;
+      switch (wybor)
+      {
+      case 'r':
+      {
+          cout << endl
+               << "Podaj wartość kąta (wznoszenia/opadania) w stopniach" << endl;
+          cout << "Wartość kąta:     //JESZCZE NIE DZIAŁA" << endl;
+          cout << endl
+               << "Podaj wartość odległości, na którą ma się przemiescić dron" << endl;
+          cout << "Wartość odległości:   ";
+          cin >> odlegloscX;
+          cout << endl;
 
-      cuboid.rotateZ(45);
-      cuboid.rysuj(kDroneFile);
-      Lacze.Rysuj();
+          Wektor3D change(odlegloscX, 0, 0);
 
-
-  //Lacze.Rysuj(); // Gnuplot rysuje to co jest w pliku
-  cout << "Nacisnij ENTER, aby kontynuowac ... " << flush;
-  cin >> noskipws >> c;
-
-
-  Wektor3D change(50, 0, 0);
-
-  for (int i = 0; i < 20; i++)
-  {
-      cuboid.translate(change/20);
-      cuboid.rysuj(kDroneFile);
-      Lacze.Rysuj(); 
-      chrono::milliseconds timespan(50);
-      this_thread::sleep_for(timespan);
-  }
-
-  //Lacze.Rysuj(); // Gnuplot rysuje to co jest w pliku
-  cout << "Nacisnij ENTER, aby kontynuowac ... " << flush;
-  cin >> noskipws >> c;
+          for (int i = 0; i < 20; i++)
+          {
+              cuboid.translate(change / 20);
+              cuboid.rysuj(kDroneFile);
+              Lacze.Rysuj();
+              chrono::milliseconds timespan(50);
+              this_thread::sleep_for(timespan);
+          }
+          break;
+      }
+      case 'o':
+      {
+          cout << "Podaj wartość kąta obrotu w stopniach" << endl;
+          cout << "Wartość kąta:  ";
+          cin >> a;
+          /*for (int i = 0; i < 16; i++)
+          {
+              cuboid.rotateZ(90 / 16);
+              cuboid.rysuj(kDroneFile);
+              Lacze.Rysuj();
+              chrono::milliseconds timespan(50);
+              this_thread::sleep_for(timespan);
+          }*/
+          cuboid.rotateZ(a);
+          cuboid.rysuj(kDroneFile);
+          Lacze.Rysuj();
+          break;
+      }
+      case 'm':
+      {
+          cout << endl
+               << endl
+               << "#####################################\n";
+          cout << "#               MENU:               #\n";
+          cout << "#   r - Zadaj ruch na wprost        #\n";
+          cout << "#   o - Zadaj zmianę orientacji     #\n";
+          cout << "#   m - wyświetl menu               #\n";
+          cout << "#                                   #\n";
+          cout << "#   k - Zakoncz działanie programu  #\n";
+          cout << "#####################################\n";
+          break;
+      }
+      case 'k':
+      {
+          cout << "Koniec działania programu" << endl;
+          exit(0);
+          break;
+      }
+      default:
+      {
+          cout << "Nie ma takiej opcji. Prosze sprobowac ponownie" << endl
+               << endl;
+          break;
+      }
+      } /*switch*/
+  } while (true);
 }
